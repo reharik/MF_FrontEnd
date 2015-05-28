b94b338f7b9250eec0d44fe248a1c8598726c84c
reharik <reharik@gmail.com>
2015-05-27 21:58:35 -0500

    git ignore reapplied

--- build/components/gridConcerns/GridLinkComponent.js
+++
@@ -1,8 +0,0 @@
-var React = require("react");
-
-
-var GridLinkComponent = React.createClass({displayName: "GridLinkComponent",
-    render: function(){
-        return React.createElement("a", {href: "##"})
-    }
-});
--- node_modules/react-router-bootstrap/lib/index.js
+++
@@ -1,9 +0,0 @@
-var ButtonLink = require('./ButtonLink');
-var MenuItemLink = require('./MenuItemLink');
-var NavItemLink = require('./NavItemLink');
-
-module.exports = {
-  ButtonLink: ButtonLink,
-  MenuItemLink: MenuItemLink,
-  NavItemLink: NavItemLink
-};
