d6105750fa490675451d1e450da60a749522c4a9
Raif Harik <reharik@gmail.com>
2016-11-04 15:56:24 -0500

    cleaning up input a bit

--- app/src/components/formElements/Input.js
+++ app/src/components/formElements/Input.js
@@ -1,34 +1,39 @@
 import React from 'react';
 import InputColor from 'react-input-color';
-// import propToLabel from './../../utilities/propToLabel';
 import TokenAutocomplete from '../formElements/reactSelect/index';
-// import uuid from 'uuid';
 import Datepicker from 'react-datepicker';
 import moment from 'moment';
 import MaskedInput from 'react-maskedinput'
+import classNames from 'classnames'
 
 const _Input = ({data,
-                validation, 
+                validation,
                 containerStyle,
                 selectOptions,
                 notifs, 
                 notifSend, 
                 notifDismiss}) => {
-  console.log('==========data=========');
-  console.log(data);
-  console.log('==========END data=========');
-  let validationState = data.invalid ?  'input__success' : 'input__error';
-  let style = 'input__container__' + (data.type ? data.type : 'input') + ' ' + validationState;
-  let val = data.errors.length > 0 ? data.error : '';
-  let valStyle = data.errors.length > 0
-    ? 'input__container__validation__error'
-    : 'input__container__validation__success';
+
+  let inputStyle = classNames({
+      ['input__container__' + (data.type ? data.type : 'input')]:true,
+      'input__success' : !data.invalid,
+      'input__error' : data.invalid
+  });
+
+  let validationStyle = classNames({
+    'input__container__validation__error' : data.errors.length > 0,
+    'input__container__validation__success' : data.errors.length == 0
+  });
+
+  const _containerStyle = classNames('input__container', containerStyle);
+
   let validationEl = null;
   switch (validation) {
     case 'inline':
     {
       // if you use inline you'll need to adjust the height of the input container
-      validationEl = (<div className={valStyle}>{val}</div>);
+      let val = data.errors.length > 0 ? data.error : '';
+      validationEl = (<div className={validationStyle}>{val}</div>);
       break;
     }
     case 'top':
@@ -54,7 +59,9 @@ const _Input = ({data,
     //   // dispatch(notifDismiss(data.name));
   }
 
-  const _containerStyle = containerStyle ? containerStyle : '';
+  data.label = `${data.label}${
+    data.label.indexOf('*') <0 
+    && data.rules.some(x=>x.rule=='required') ? '*' : ''}`;
 
   const input = function() {
     switch(data['x-input'] || data.type){
@@ -64,13 +71,13 @@ const _Input = ({data,
                             {...data}
                             onChange={onChange}
                             onBlur={data.onBlur}
-                            className={style} />)
+                            className={inputStyle} />)
       }
       case 'color-picker': {
         return (<InputColor {...data} defaultValue="#345678" />)
       }
       case 'select': {
-        return (<TokenAutocomplete className={style} simulateSelect={true}
+        return (<TokenAutocomplete className={inputStyle} simulateSelect={true}
                                    parseToken={ value => value.label }
                                    parseOption={ value => value.label }
                                    options={selectOptions} {...data}
@@ -78,7 +85,7 @@ const _Input = ({data,
                                    {...data} />)
       }
       case 'multi-select': {
-        return (<TokenAutocomplete className={style}
+        return (<TokenAutocomplete className={inputStyle}
                                    defaultValues={data.value || []}
                                    parseToken={ value => value.label }
                                    parseOption={ value => value.label }
@@ -87,8 +94,11 @@ const _Input = ({data,
       }
       default:
       case 'number':
+      case 'password':
       case 'string': {
-        return (<input className={style}
+        const password = data['x-input'] === 'password' ? {type: "password"} : '';
+        return (<input className={inputStyle}
+          {...password}
                        placeholder={data.placeholder}
                        name={data.name}
                        value={data.value}
@@ -98,7 +108,7 @@ const _Input = ({data,
     }
   };
 
-  return (<div className={"input__container " + _containerStyle} >
+  return (<div className={_containerStyle} >
     <label className="input__container__label" htmlFor={data.name}>{data.label}</label>
     {input()}
     {/*{validationEl}*/}
